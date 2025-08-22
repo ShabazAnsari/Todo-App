d47f69af63db43637d417911458c1dd3432669aa
MD SHABAZ ANSARI <124130546+ShabazAnsari@users.noreply.github.com>
2025-08-22 20:30:50 +0530

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const API_URL = "http://localhost:3000/api"; // Change if backend hosted elsewhere
+  const API_URL = "https://todo-app-wa7a.onrender.com/api"; 
 
   // Auth elements
   const authSection = document.getElementById("authSection");
